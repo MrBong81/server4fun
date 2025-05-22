753c9adb7676385ec79f5140010b58051a725792
MrBong <thepowerofthebong@gmail.com>
2025-05-22 18:29:52 +0200

    Homepage default; implemented simple server; updated makefile

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -12,6 +14,14 @@ func main() {
 
 	port := os.Getenv("PORT")
 
+	router := http.NewServeMux()
+
+	router.HandleFunc("/", ServeHome)
 	fmt.Println("Welcome to the backend jungle")
 	fmt.Printf("Secret port: %v\n", port)
+	log.Fatal(http.ListenAndServe(":6969", router))
+}
+
+func ServeHome(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./src/homepage_tml.html")
 }
